568851b2c9ac7c44098f751fe728e0c22184d8e3
DAVI BRITO SILVA E SOUZA <davi.brito@a.ucb.br>
2024-04-19 10:50:45 -0300

    afk

---
+++ autogui/afk.py
@@ -0,0 +1,21 @@
+import keyboard as k
+import pyautogui as pa
+import time as t
+
+t.sleep(5)
+k.block_key('f4')
+t.sleep(.1)
+k.block_key('win')
+t.sleep(.1) 
+k.block_key('ctrl')
+t.sleep(.1)
+k.block_key('alt')
+t.sleep(.1)
+k.block_key('tab')
+t.sleep(.1)
+k.block_key('enter')
+t.sleep(.1)
+pa.moveTo(0,700,10000)
+
+
+
